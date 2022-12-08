b5fb77cab20b05ec37f1c7772314f452c860c4ff
christopher <bertonzin@outlook.com>
2022-12-07 21:54:09 -0300

    day 0

---
+++ amigosecreto/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Participante, Presente
+
+admin.site.register(Participante)
+admin.site.register(Presente)
--- amigosecreto/models.py
+++ amigosecreto/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 
+
 class Participante(models.Model):
-    nome = models.CharField()
-    cod = models.IntegerField()
-    presentes = models.ForeignKey()
-    amigo = models.IntegerField()
+    cod = models.IntegerField(primary_key=True,)
+    nome = models.CharField(max_length=20)
+    cod_amigo = models.IntegerField()
 
 
 class Presente(models.Model):
-    descricao = models.CharField()
-    tamanho = models.CharField()
-    link = models.CharField()
-    observacoes = models.CharField()
+    participante = models.ForeignKey(Participante, on_delete=models.CASCADE)
+    descricao = models.CharField(max_length=200)
+    tamanho = models.CharField(max_length=2)
+    link = models.CharField(max_length=500)
+    observacoes = models.CharField(max_length=500)
+
+class Sorteados(models.Model):
+    cod_participante = models.IntegerField()
